d002a97fc720df0480d7b387c365bc892864f864
Sefa YILDIZ <sefayildiz@hotmail.com.tr>
2018-04-30 15:58:40 +0300

    Main controller

--- src/Controller/CurrencyController.php
+++ src/Controller/CurrencyController.php
@@ -15,7 +15,7 @@ class CurrencyController extends Controller
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        // curl_setopt($ch,CURLOPT_HEADER, false);
+        // curl_setopt($ch,CURLOPT_HEADER, fæalse);
         $output = curl_exec($ch);
         curl_close($ch);
         return json_decode($output);
